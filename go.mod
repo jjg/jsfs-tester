1c4d58816f1dececddb42312d25c84a4a8c90653
Jason J. Gullickson <mr@jasongullickson.com>
2024-09-11 10:08:07 -0500

    Initial sketch of smoke test program.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jjg/jsfsmoke
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "log"
+  "fmt"
+)
+
+func main() {
+  log.Print("Starting up...")
+
+  // DEBUG
+  fmt.Println("Hello!")
+  
+  // TODO: parse arguments for server name, port, concurrency, 
+  // number of requests, verbosity, etc.
+  
+  log.Print("All done!")
+}
